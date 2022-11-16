cc7b2ac46e4cbb7b061d5eea4f84099db54d272e
ANN KARUKU <annkaruku5832@gmail.com>
2022-11-16 17:16:53 +0300

    Added splash screen and changed the launcher icon.

--- app/src/main/java/com/example/ewaykollect/MainActivity.kt
+++ app/src/main/java/com/example/ewaykollect/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.ewaykollect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //splash screen delay for 3 seconds
+        Thread.sleep(1000)
+
+        //install the splash screen to the main activity on launch
+        installSplashScreen()
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }

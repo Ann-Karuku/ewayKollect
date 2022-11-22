f6a49f251dfd46b15111206e9a4f84f422fc3f9e
ANN KARUKU <annkaruku5832@gmail.com>
2022-11-22 20:39:40 +0300

    Added variables to access the UI login and registration elements.

--- app/src/main/java/com/example/ewaykollect/UserLogin.kt
+++ app/src/main/java/com/example/ewaykollect/UserLogin.kt
@@ -1,11 +1,33 @@
 package com.example.ewaykollect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class UserLogin : AppCompatActivity() {
+
+    private lateinit var uname : EditText
+    private lateinit var password: EditText
+    private lateinit var signUpBtn: Button
+    private lateinit var registerLink: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_login)
+
+
+        uname= findViewById(R.id.edtUname)
+        password=findViewById(R.id.edtPassword)
+        signUpBtn=findViewById(R.id.signUpBtn)
+        registerLink=findViewById(R.id.regLink)
+
+
+        registerLink.setOnClickListener{
+            val intent=Intent(this,UserRegistration::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/ewaykollect/UserRegistration.kt
+++ app/src/main/java/com/example/ewaykollect/UserRegistration.kt
@@ -1,11 +1,36 @@
 package com.example.ewaykollect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class UserRegistration : AppCompatActivity() {
+
+    private lateinit var uname : EditText
+    private lateinit var phone : EditText
+    private lateinit var password: EditText
+    private lateinit var passwordRpt: EditText
+    private lateinit var signInBtn: Button
+    private lateinit var loginLink: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_registration)
+
+        uname= findViewById(R.id.edtUname)
+        phone=findViewById(R.id.edtPhone)
+        password=findViewById(R.id.edtPassword)
+        passwordRpt=findViewById(R.id.edtRepeatPass)
+        signInBtn=findViewById(R.id.signUpBtn)
+        loginLink=findViewById(R.id.loginLink)
+
+        loginLink.setOnClickListener {
+            val intent= Intent(this,UserLogin::class.java)
+            startActivity(intent)
+        }
     }
 }

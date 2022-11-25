7000696f65fddfe7168675aab53e8d48f3b1f034
ANN KARUKU <annkaruku5832@gmail.com>
2022-11-25 17:10:39 +0300

    Added firebase authentication email/password on UserRegistration.kt.

--- app/src/main/java/com/example/ewaykollect/UserRegistration.kt
+++ app/src/main/java/com/example/ewaykollect/UserRegistration.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class UserRegistration : AppCompatActivity() {
 
@@ -17,7 +20,7 @@ class UserRegistration : AppCompatActivity() {
     private lateinit var signInBtn: Button
     private lateinit var loginLink: TextView
 
-    private lateinit var mAuth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,22 +34,45 @@ class UserRegistration : AppCompatActivity() {
         signInBtn=findViewById(R.id.signUpBtn)
         loginLink=findViewById(R.id.loginLink)
 
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
         loginLink.setOnClickListener {
             val intent= Intent(this,UserLogin::class.java)
             startActivity(intent)
         }
 
         signInBtn.setOnClickListener{
-//            perfomAuth()
+            perfomAuth()
             val intent=Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
+
+
+
     }
 
-//    private fun perfomAuth{
-//        var email: String=email.text.toString()
-////      var phone :Int= phone.
-//        var password:String=password.text.toString()
-//        var passwordRpt:String=passwordRpt.text.toString()
-//    }
+    private fun perfomAuth() {
+        var email: String = email.text.toString()
+//      var phone :Int= phone.
+        var password: String = password.text.toString()
+        var passwordRpt: String = passwordRpt.text.toString()
+
+        if (password===passwordRpt) {
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(this,"Registration success..",Toast.LENGTH_LONG,)
+
+                    } else {
+                        // If sign up fails, display a message to the user.
+                        Toast.makeText(this,"Authentication failed.",Toast.LENGTH_LONG,)
+
+                    }
+                }
+        }
+    }
+
 }

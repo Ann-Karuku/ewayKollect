64ed10bdac5029a1fd62d174589029fd78400435
ANN KARUKU <annkaruku5832@gmail.com>
2022-12-10 18:33:18 +0300

    Added a facebook SDK login dependency on build.gradle.

--- app/src/main/java/com/example/ewaykollect/notificationsFragment.kt
+++ app/src/main/java/com/example/ewaykollect/notificationsFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-class notificationsFragment : Fragment() {
+class NotificationsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,

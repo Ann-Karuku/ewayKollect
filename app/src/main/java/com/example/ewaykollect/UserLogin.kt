43e74d0c7754bedec3988a427c6360dead1ad1ef
ANN KARUKU <annkaruku5832@gmail.com>
2022-12-14 16:36:30 +0300

    Added facebook authentication configuration on UserLogin.kt.

--- app/src/main/java/com/example/ewaykollect/UserLogin.kt
+++ app/src/main/java/com/example/ewaykollect/UserLogin.kt
@@ -1,21 +1,32 @@
 package com.example.ewaykollect
 
 import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.facebook.*
+import com.facebook.appevents.AppEventsLogger
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import java.security.MessageDigest
 
 class UserLogin : AppCompatActivity() {
 
@@ -24,9 +35,12 @@ class UserLogin : AppCompatActivity() {
     private lateinit var signUpBtn: Button
     private lateinit var registerLink: TextView
     private lateinit var googleBtn: ImageView
+    private lateinit var fbBtn: LoginButton
 
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+
+    private lateinit var callbackManager: CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,39 +52,91 @@ class UserLogin : AppCompatActivity() {
 
         setContentView(R.layout.activity_user_login)
 
+        printHashKey(applicationContext)
+
+
 
         email = findViewById(R.id.edtEmail)
         password = findViewById(R.id.edtPassword)
         signUpBtn = findViewById(R.id.signUpBtn)
         registerLink = findViewById(R.id.regLink)
         googleBtn=findViewById(R.id.google)
+        fbBtn=findViewById(R.id.btn_facebook)
 
         // Initialize Firebase Auth
         auth = Firebase.auth
 
+        // Initialize Facebook Login button
+        callbackManager = CallbackManager.Factory.create()
+        fbBtn.setReadPermissions("email", "public_profile")
+        fbBtn.setOnClickListener {
+            fbSignin()
+        }
+
+
 
         registerLink.setOnClickListener {
-            val intent = Intent(this, UserRegistration::class.java)
-            startActivity(intent)
-        }
+                val intent = Intent(this, UserRegistration::class.java)
+                startActivity(intent)
+            }
 
         signUpBtn.setOnClickListener {
-            perfomAuth()
+                perfomAuth() }
 
-        }
-
-        //google authentication
+            //google authentication
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
 
-        googleSignInClient = GoogleSignIn.getClient(this , gso)
+            googleSignInClient = GoogleSignIn.getClient(this , gso)
 
-        googleBtn.setOnClickListener {
-            signInGoogle()
+            googleBtn.setOnClickListener {
+                signInGoogle()
+            }
         }
 
+
+
+    private fun fbSignin() {
+        fbBtn.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+            }
+
+            override fun onError(error: FacebookException) {
+                Log.d(TAG, "facebook:onError", error)
+            }
+        })
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:$token")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+//                    Toast.makeText(this,"")
+                    val intent : Intent = Intent(this , MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data)
     }
 
     //method to open home activity after google auth
@@ -138,5 +204,25 @@ class UserLogin : AppCompatActivity() {
                     }
                 }
         }
+    }
+
+    //facebook login
+    private fun printHashKey(context: Context) {
+
+        try {
+            val info = context.packageManager.getPackageInfo(
+                context.packageName,
+                PackageManager.GET_SIGNATURES
+            )
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey = String(Base64.encode(md.digest(), 0))
+                Log.i("AppLog", "key:$hashKey=")
+            }
+        } catch (e: Exception) {
+            Log.e("AppLog", "error:", e)
+        }
+
     }
 }

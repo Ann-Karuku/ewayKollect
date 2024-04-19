1bcfdeec1e2eb365bde3b03f6527dcd995e028cf
karuku-design <annkaruku5832@gmail.com>
2024-04-19 17:06:20 +0300

    Removed code for display user info.

--- app/src/main/java/com/example/ewaykollect/accountFragment.kt
+++ app/src/main/java/com/example/ewaykollect/accountFragment.kt
@@ -30,33 +30,8 @@ class AccountFragment : Fragment() {
 
         var root:View=inflater.inflate(R.layout.fragment_account, container, false)
 
-        acc_image=  root.findViewById<ImageView>(R.id.acc_image)
-        acc_image.setOnClickListener{
-            Navigation.findNavController(root).navigate(R.id.action_accountFragment_to_profileFragment)
-        }
-
-        val userID= FirebaseAuth.getInstance().currentUser!!.uid
-
-        val ref=db.collection("user").document(userID)
-
-        ref.get().addOnSuccessListener {
-            if(it!=null){
-                val name= it.data?.get("name").toString()
-                val email= it.data?.get("email").toString()
-                val image=it.data?.get("image").toString()
 
 
-
-                var dpName=root.findViewById<TextView>(R.id.acc_name)
-                dpName.setText(name)
-                var dpmail=root.findViewById<TextView>(R.id.acc_email)
-                dpmail.setText(email)
-                Glide.with(this.context).load(image).into(acc_image)
-            }
-        }
-            .addOnFailureListener{
-                Toast.makeText(this.context, "Failed to load data", Toast.LENGTH_LONG).show()
-            }
 
         return root
 

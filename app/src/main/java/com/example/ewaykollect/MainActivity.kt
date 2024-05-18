6bec9e44f17c8fbe41225714c845220c123cf443
mkaomwakuni <mkaoibrahim@gmail.com>
2024-05-18 17:21:18 +0300

    Resolved Navigation Action Issue

--- app/src/main/java/com/example/ewaykollect/MainActivity.kt
+++ app/src/main/java/com/example/ewaykollect/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.ewaykollect
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
@@ -9,16 +8,15 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.net.toUri
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bumptech.glide.Glide
 import com.google.android.material.navigation.NavigationView
@@ -27,120 +25,102 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener{
-
-    private lateinit var drawer : DrawerLayout
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var auth: FirebaseAuth
+    private val db = Firebase.firestore
+    private lateinit var drawer: DrawerLayout
     private lateinit var navController: NavController
-
-    //for firebase authentication
-    private lateinit var auth : FirebaseAuth
-
-    private var db=Firebase.firestore
-
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-
-        //find the toolbar
-        var toolbar: Toolbar = findViewById(R.id.toolBar)
-        setSupportActionBar(toolbar)
-
-        //find the drawer layout
-        drawer = findViewById(R.id.drawerLyt)
-
-        //toogle the navDrawer using a hamburger icon.
-        var toogle: ActionBarDrawerToggle = ActionBarDrawerToggle(this, drawer, toolbar,
-            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-
-        //add a toogle listener
-        drawer.addDrawerListener(toogle)
-        toogle.syncState()
-
-        //navigation view
-        var nav_view: NavigationView = findViewById(R.id.nav_view)
-        nav_view.setNavigationItemSelectedListener(this)
-
-        // Find the NavHostFragment
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-
-
-
-        //set default fragment as home
-        if (savedInstanceState==null){
-            replaceFragment(HomeFragment())
-            nav_view.setCheckedItem(R.id.nav_home)
-        }
-
 
         // Initialize Firebase Auth
         auth = Firebase.auth
 
-        //google firebase auth details fetch
-        val userID= FirebaseAuth.getInstance().currentUser!!.uid
+        // Initialize toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolBar)
+        setSupportActionBar(toolbar)
 
-        val ref=db.collection("user").document(userID)
-        ref.get().addOnSuccessListener {
-            if(it!=null){
-                val name= it.data?.get("name").toString()
-                val email= it.data?.get("email").toString()
-                val phone= it.data?.get("phone").toString()
-                val password= it.data?.get("password").toString()
-                val image=it.data?.get("image").toString()
+        // Initialize drawer layout
+        drawer = findViewById(R.id.drawerLyt)
 
+        // Initialize Navigation Component
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
-                var nav_view:NavigationView=findViewById(R.id.nav_view)
-                var header_view: View =nav_view.getHeaderView(0)
-                var dpName=header_view.findViewById<TextView>(R.id.nav_name)
-                dpName.setText(name)
-                var dpmail=header_view.findViewById<TextView>(R.id.nav_email)
-                dpmail.setText(email)
-                var dpImage=header_view.findViewById<ImageView>(R.id.nav_image)
-                Glide.with(this).load(image).into(dpImage)
-            }
-        }
-            .addOnFailureListener{
+        // Set up ActionBar with Navigation Controller
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.nav_home, R.id.nav_account, R.id.nav_notifications, R.id.nav_profile, R.id.nav_settings
+        ), drawer)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Set up Navigation View with NavController
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        navView.setupWithNavController(navController)
+
+        // Add a toggle listener
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+
+        // Fetch Firebase auth details
+        val userID = auth.currentUser?.uid
+        userID?.let {
+            val ref = db.collection("user").document(it)
+            ref.get().addOnSuccessListener { document ->
+                if (document != null) {
+                    val name = document.getString("name")
+                    val email = document.getString("email")
+                    val image = document.getString("image")
+
+                    val headerView: View = navView.getHeaderView(0)
+                    val dpName = headerView.findViewById<TextView>(R.id.nav_name)
+                    val dpMail = headerView.findViewById<TextView>(R.id.nav_email)
+                    val dpImage = headerView.findViewById<ImageView>(R.id.nav_image)
+
+                    dpName.text = name
+                    dpMail.text = email
+                    Glide.with(this).load(image).into(dpImage)
+                }
+            }.addOnFailureListener {
                 Toast.makeText(this, "Failed to load data", Toast.LENGTH_SHORT).show()
             }
-    }
-
-    private fun replaceFragment(fragment:Fragment) {
-        val transaction :FragmentTransaction =supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container,fragment)
-        transaction.commit()
-    }
-
-    override fun onBackPressed() {
-
-        if (drawer.isDrawerOpen(GravityCompat.START)){
-            drawer.closeDrawer(GravityCompat.START)
-        }else{
-            onBackPressedDispatcher.onBackPressed()
         }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-
-        when (item.itemId){
-            R.id.nav_home->replaceFragment(HomeFragment())
-            R.id.nav_account->replaceFragment(AccountFragment())
-            R.id.nav_notifications->replaceFragment(NotificationsFragment())
-            R.id.nav_profile->replaceFragment(ProfileFragment())
-            R.id.nav_settings->replaceFragment(SettingsFragment())
-            R.id.nav_FAQs->replaceFragment(FAQsFragment())
-            R.id.nav_logOut->{
-            Firebase.auth.signOut()
-            startActivity(Intent(this, UserLogin::class.java))
-                Toast.makeText(this, "logout!", Toast.LENGTH_SHORT).show()
+        when (item.itemId) {
+            R.id.nav_home -> navController.navigate(R.id.homeFragment)
+            R.id.nav_account -> navController.navigate(R.id.accountFragment)
+            // Other layouts here
+            R.id.nav_logOut -> {
+                auth.signOut()
+                startActivity(Intent(this, UserLogin::class.java))
+                Toast.makeText(this, "Logged out!", Toast.LENGTH_SHORT).show()
+                finish()
+            }
         }
-       }
 
         drawer.closeDrawer(GravityCompat.START)
         return true
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 
-
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
 }
--- app/src/main/java/com/example/ewaykollect/accountFragment.kt
+++ app/src/main/java/com/example/ewaykollect/accountFragment.kt
@@ -1,28 +1,28 @@
 package com.example.ewaykollect
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 class AccountFragment : Fragment() {
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val root:View = inflater.inflate(R.layout.fragment_account, container, false)
+    ): View {
+        val root: View = inflater.inflate(R.layout.fragment_account, container, false)
 
-        // navigate to myEwaste fragment
         val myEwasteItems = root.findViewById<CardView>(R.id.cdvw_my_ewaste)
         myEwasteItems.setOnClickListener {
-            findNavController().navigate(R.id.action_accountFragment2_to_homeFragment2)
+            // Check if the current destination is AccountFragment
+            if (findNavController().currentDestination?.id == R.id.accountFragment) {
+                // Navigate to HomeFragment using the generated action
+                findNavController().navigate(R.id.action_accountFragment_to_homeFragment)
+            }
         }
         return root
     }

826bd2334d32dc1083f7873223604d054f9cb7c0
karuku-design <annkaruku5832@gmail.com>
2024-08-11 21:36:18 +0300

    Added functionality for floating btn on MyEwasteFragment

--- app/src/main/java/com/example/ewaykollect/myEwaste.kt
+++ app/src/main/java/com/example/ewaykollect/myEwaste.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 class MyEwasteFragment : Fragment() {
 
 
@@ -12,10 +15,20 @@ class MyEwasteFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+
+        val root: View = inflater.inflate(R.layout.fragment_my_ewaste, container, false)
+        // Find the floating action button
+        val fabAddItem = root.findViewById<FloatingActionButton>(R.id.fabAddItem)
+
+        // Set an OnClickListener to navigate to the AddEwasteDialogFragment
+        fabAddItem.setOnClickListener {
+            // Use NavController to navigate to the desired fragment
+            findNavController().navigate(R.id.action_myEwaste_to_addEwasteDialogFragment)
+        }
 
 
-        return inflater.inflate(R.layout.fragment_my_ewaste, container, false)
+        return root
     }
 
 

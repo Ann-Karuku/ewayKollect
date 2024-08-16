6e926085b980b47539cccc974ad1f7046174db5b
karuku-design <annkaruku5832@gmail.com>
2024-08-16 13:01:06 +0300

    Created an array for the spinner.

--- app/src/main/java/com/example/ewaykollect/AddEwasteFragment.kt
+++ app/src/main/java/com/example/ewaykollect/AddEwasteFragment.kt
@@ -8,22 +8,42 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.Toast
 
 class AddEwasteDialogFragment : DialogFragment() {
-
-
+    // Categorized list of e-waste items
+    private val items = arrayOf(
+        "Mobile Phones", "Tablets", "Laptops",
+        "Desktop Computers", "Monitors", "Printers",
+        "Televisions", "Remote Controls", "DVD Players",
+        "Washing Machines", "Refrigerators", "Microwaves",
+        "Air Conditioners", "Electric Fans", "Heaters"
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate your custom layout for the dialog
+        val root: View = inflater.inflate(R.layout.fragment_add_ewaste, container, false)
 
+        // Find the Spinner in the inflated layout
+        val spinner = root.findViewById<Spinner>(R.id.spinnerType)
 
+        // Creating an ArrayAdapter using the EwasteItems array and a custom layout
+        val arrayAdapter = ArrayAdapter(
+            requireContext(),
+            R.layout.spinner_item,
+            items
+        )
 
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = arrayAdapter
 
-        return inflater.inflate(R.layout.fragment_add_ewaste, container, false)
+        return root
     }
 }

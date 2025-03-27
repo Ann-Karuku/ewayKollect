fa6582194341c96c305b723d21b64c1ab00a9d1a
Ann Karuku <annkaruku5832@gmail.com>
2025-03-27 15:02:53 +0300

    ft: add change password fragment with UI

---
+++ app/src/main/java/com/example/ewaykollect/ChangePasswordFragment.kt
@@ -0,0 +1,21 @@
+package com.example.ewaykollect
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ChangePasswordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val root=
+        return inflater.inflate(R.layout.fragment_change_password, container, false)
+    }
+
+}

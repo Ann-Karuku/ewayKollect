cab42422165dc0f49db4b4020787dcfc78c5da42
Ann Karuku <annkaruku5832@gmail.com>
2025-03-27 11:54:02 +0300

    ft: add edit profile module

---
+++ app/src/main/java/com/example/ewaykollect/EditProfileFragment.kt
@@ -0,0 +1,27 @@
+package com.example.ewaykollect
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EditProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val root=inflater.inflate(R.layout.fragment_edit_profile, container, false)
+
+
+
+
+
+
+
+        return root
+    }
+
+}

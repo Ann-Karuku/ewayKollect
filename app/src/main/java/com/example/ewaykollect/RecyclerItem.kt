c6c35504cc9e5ffe8a2ea35f556babb7fb800a4f
Ann Karuku <annkaruku5832@gmail.com>
2025-04-22 16:23:19 +0300

    bf: change to serializable to solve the crash error when navigating to view company details

--- app/src/main/java/com/example/ewaykollect/RecyclerItem.kt
+++ app/src/main/java/com/example/ewaykollect/RecyclerItem.kt
@@ -1,5 +1,7 @@
 package com.example.ewaykollect
 
+import java.io.Serializable
+
 data class RecyclerItem(
     val id: String,
     val name: String,
@@ -7,7 +9,12 @@ data class RecyclerItem(
     val acceptedTypes: List<String>,
     val contact: String,
     val rating: Double,
-    val coordinates: Pair<Double, Double>,
+    val coordinates: Coordinates,
     val popularityScore: Int,
     val logoUrl: String? = null
-)
+) : Serializable
+
+data class Coordinates(
+    val longitude: Double,
+    val latitude: Double
+) : Serializable
